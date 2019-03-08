b9fb4828fb82992abbd032ac627375c65e90f74b
Lauri Räsänen <pancakelarry@gmail.com>
2019-03-08 23:40:30 +0200

    Update startup script to include python_sr and repo cloning

--- TV.bat
+++ TV.bat
@@ -1,10 +1,41 @@
-cd /d "..\DemoTools"
+:: python song request
+cd /d %~dp0\..
+IF NOT EXIST python_sr (
+	git clone https://github.com/laurirasanen/python_sr
+)
+cd /d ".\python_sr"
+
+:: update song request
+git pull
+call pip install -r ./requirements.txt
+
+:: run song request
+start "python_sr" cmd /c "python ./sr.py"
+
+
+:: demo tools
+cd /d %~dp0\..
+IF NOT EXIST .\demotools (
+	git clone https://github.com/laurirasanen/demotools
+)
+cd /d ".\demotools"
+
+:: update demo tools
 git pull
 call npm i
 
-cd /d "..\TempusTV"
+
+:: tempustv
+cd /d %~dp0\..
+IF NOT EXIST .\tempustv (
+	git clone https://github.com/laurirasanen/tempustv
+)
+cd /d ".\tempustv"
+
+:: update tempustv
 git pull
 call npm i
 
+:: run tempustv
 node "app.js"
 pause
