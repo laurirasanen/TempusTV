3722710d91c22586e87f59cd093edfc81f013954
laurirasanen <laurirasanen@occasionally.cool>
2019-05-07 23:02:43 +0300

    Fix milliseconds missing leading 0 in timestamps

--- utils.js
+++ utils.js
@@ -8,6 +8,7 @@
     var hourstr = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
+    milliseconds = (milliseconds < 100) ? "0" + milliseconds : milliseconds;
 
     if (hours > 0)
         return `${hourstr}:${minutes}:${seconds}.${milliseconds}`;
